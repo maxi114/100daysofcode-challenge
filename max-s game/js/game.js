b94e1ee951d078439734bebca18884cff5ca5a72
Maximillian Omondi Kodi <mokodi114@gmail.com>
2018-01-08 16:26:19 -0500

    Add files via upload

--- max-s game/js/game.js
+++ max-s game/js/game.js
@@ -1,60 +1,18 @@
 var canvas = document.getElementById('canvas');
 
+//sets canvas to full width and hight of the window/screen
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+//draw in 2d function
 var c = canvas.getContext('2d');
 
-/*c.fillStyle = "red";
-c.fillRect(200, 200, 100, 100);
+// building
+c.fillStyle = "yellow"
+c.fillRect(50, 50, 100, 200);
+c.stroke();
 
-//line
-c.beginPath();
-c.moveTo(200, 50);
-c.lineTo(300, 50);
-c.strokeStyle = "red"
-c.stroke();*/
-
-//arc/circle object
-function Circle(x, y) {
-    this.x = x;
-    this.y = y;
-
-    this.draw = function() {
-        c.beginPath();
-        c.arc(x, y, 30, 0, Math.PI * 2);
-        c.stroke();
-    }
-}
-
-var circle = new Circle(200, 200);
-
-
-
-//for loop to draw multiple circles randomly
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5 * 8);
-var dy = (Math.random() - 0.5 * 8);
-var radius = 30;
-
-function animation() {
-    requestAnimationFrame(animation);
-
-    c.clearRect(0, 0, innerWidth, innerHeight);
-    circle.draw();
-    c.beginPath();
-
-    c.arc(x, y, 30, 0, Math.PI * 2);
-    c.stroke();
-    if (x + radius > innerWidth || x - radius < 0) {
-        dx = -dx;
-    }
-    if (y + radius > innerHeight || y - radius < 0) {
-        dy = -dy;
-    }
-    x += dx;
-    y += dy;
-
-}
-animation();
+//windows
+c.fillStyle = "blue"
+c.fillRect(50, 50, 20, 20);
+c.stroke();
